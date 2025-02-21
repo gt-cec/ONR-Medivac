a7a6550e971b089f31f547617a106d6b22a5a761
Jack Kolb <jack@jackkolb.com>
2025-02-21 15:28:33 -0500

    moving Jarvis back to TTS

--- Server/Jarvis.py
+++ Server/Jarvis.py
@@ -1,16 +1,27 @@
-from yapper import Yapper, PiperSpeaker, PiperVoiceUS, DefaultEnhancer, Persona
+# from yapper import Yapper, PiperSpeaker, PiperVoiceUS, DefaultEnhancer, Persona
 
-speaker = PiperSpeaker(
-    voice=PiperVoiceUS.KUSAL
-)
+# speaker = PiperSpeaker(
+#     voice=PiperVoiceUS.KUSAL
+# )
 
-# Initialize TTS
-yapper = Yapper(speaker=speaker)
+# # Initialize TTS
+# yapper = Yapper(enhancer=DefaultEnhancer(Persona.JARVIS))
 
-# Global flag for stopping speech
-current_speech_task = None
+# # Global flag for stopping speech
+# current_speech_task = None
 
-def speak(text, speaker_name):
-    """Generate and play speech"""
-    global current_speech_task
-    yapper.yap(text, plain=True)
+# def speak(text, speaker_name):
+#     """Generate and play speech"""
+#     global current_speech_task
+#     yapper.yap(text, plain=True)
+
+
+import torch
+from TTS.api import TTS
+
+# Get device
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+# List available 🐸TTS models
+print(TTS().list_models())
+
