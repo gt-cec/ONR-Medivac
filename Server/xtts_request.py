bfc6b9180d4a3e37dba996c48619a959ae319d81
sanyadoda <sanyadoda28@gmail.com>
2025-04-08 17:42:44 -0400

    tts models

--- Server/xtts_request.py
+++ Server/xtts_request.py
@@ -1,2 +1,8 @@
  tts --model_name tts_models/multilingual/multi-dataset/xtts_v2 \
     --list_speaker_idx
+
+
+-model_name tts_models/en/ljspeech/tacotron2-DCA --use_cuda True
+
+https://mbarnig.github.io/TTS-Models-Comparison/
+
