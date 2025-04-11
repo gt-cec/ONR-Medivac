21fe04fdf150edc6efcd501b77ba1ac971b0cffa
ae-dagbeyibor3 <ae-dagbeyibor3@AE-KFRG-307166>
2025-04-11 17:58:45 -0400

    melo tts

--- Server/meloTTSManager.py
+++ Server/meloTTSManager.py
@@ -1,22 +1,29 @@
 import threading
 import queue
 import numpy as np
+import time
+import soundfile as sf
+import os
 import sounddevice as sd
 from melo.api import TTS
 
 
 class MelottsTTSManager:
-    def __init__(self, model_name="en_male", sample_rate=24000):
-        self.tts = TTS(model_name)
+    def __init__(self, sample_rate=24000):
+        self.tts = TTS(language='EN_V2', device="cpu")
         self.sample_rate = sample_rate
+        self.speaker_ids = self.tts.hps.data.spk2id
         self.audio_queue = queue.Queue()
         self.generator_thread = threading.Thread(target=self._process_queue, daemon=True)
         self.player_thread = threading.Thread(target=self._play_audio, daemon=True)
         self.generator_thread.start()
-        self.player_thread.start()
+        self.player_thread.start() 
+        
 
     def add_text(self, text, emotion=None):
         self.audio_queue.put((text, emotion))
+
+            
 
     def _process_queue(self):
         while True:
@@ -26,11 +33,15 @@ class MelottsTTSManager:
 
             # File output path
             timestamp = int(time.time() * 1000)
-            filename = f"output_{timestamp}.wav"
+            filename = f"output.wav"
 
             # Generate audio file
             print(f"[Generator] Generating audio for: {text}")
-            filepath = self.tts.tts_to_file(text, filename)
+            filepath = self.tts.tts_to_file(text, self.speaker_ids['EN-US'], filename, speed=1.0)
+            print(filename)
+            data, sr = sf.read(filename, dtype='float32')
+            # sd.play(data, samplerate=sr)
+            # sd.wait()
 
             # Put filepath in queue for playback
             self.audio_queue.task_done()
@@ -46,3 +57,10 @@ class MelottsTTSManager:
                 sd.wait()
                 os.remove(item)  # Clean up
                 self.audio_queue.task_done()
+
+
+""" if __name__ == "__main__":
+    melotts=MelottsTTSManager()
+    melotts.add_text(" this is a test. hi I am Jarvis ")
+    melotts._process_queue() """
+   
